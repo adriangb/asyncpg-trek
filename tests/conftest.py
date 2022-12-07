53bace05782558e8ba487c2fea2d93b7ef70cc78
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2022-12-07 08:08:27 -0600

    Add missing file

---
+++ tests/conftest.py
@@ -0,0 +1,8 @@
+from typing import Any
+import pytest
+
+@pytest.fixture(params=[
+    pytest.param(('asyncio', {'use_uvloop': False}), id='asyncio'),
+])
+def anyio_backend(request: Any) -> Any:
+    return request.param
