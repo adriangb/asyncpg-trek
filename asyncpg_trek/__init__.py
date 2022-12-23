e4596470edaa7fb7244db8da84e8564ce988e198
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2022-12-23 01:08:12 -0800

    Split SupportsBackend into SupportsBackend and SupportsBackendExecutor (#5)

--- asyncpg_trek/__init__.py
+++ asyncpg_trek/__init__.py
@@ -1,10 +1,11 @@
 from asyncpg_trek._backend import SupportsBackend
 from asyncpg_trek._run import execute, plan
-from asyncpg_trek._types import Direction
+from asyncpg_trek._types import Direction, Operation
 
 __all__ = [
     "SupportsBackend",
     "plan",
     "execute",
     "Direction",
+    "Operation",
 ]
--- asyncpg_trek/_run.py
+++ asyncpg_trek/_run.py
@@ -15,7 +15,6 @@ T = TypeVar("T")
 
 
 async def plan(
-    connection: T,
     backend: SupportsBackend[T],
     directory: Union[str, pathlib.Path],
     target_revision: str,
@@ -25,30 +24,33 @@ async def plan(
     rev_list = "\n".join([f" - {m.from_rev} -> {m.to_rev}" for m in migrations])
     logger.debug(f"Collected migrations from {directory}: {rev_list}")
     logger.debug("Creating migrations table")
-    await backend.create_table_idempotent(connection)
-    logger.debug("Getting current revision")
-    current_revision = await backend.get_current_revision(connection)
-    if current_revision:
-        logger.info(f"Current revision is {current_revision}")
-    else:
-        current_revision = "initial"
-        logger.info("No existing revisions found, starting from scratch")
-    return find_migration_path(
-        current_revision, target_revision, direction=direction, migrations=migrations
-    )
+    async with backend.connect() as exec:
+        await exec.create_table_idempotent()
+        logger.debug("Getting current revision")
+        current_revision = await exec.get_current_revision()
+        if current_revision:
+            logger.info(f"Current revision is {current_revision}")
+        else:
+            current_revision = "initial"
+            logger.info("No existing revisions found, starting from scratch")
+        return find_migration_path(
+            current_revision,
+            target_revision,
+            direction=direction,
+            migrations=migrations,
+        )
 
 
 async def execute(
-    connection: T,
     backend: SupportsBackend[T],
     plan: Sequence[Migration[T]],
 ) -> None:
-    for mig in plan:
-        logger.info(f"Running {mig.from_rev} -> {mig.to_rev}")
-        await backend.record_migration(
-            connection,
-            from_revision=mig.from_rev,
-            to_revision=mig.to_rev,
-        )
-        await mig.operation(connection)
-        logger.info(f"{mig.from_rev} -> {mig.to_rev} OK")
+    async with backend.connect() as exec:
+        for mig in plan:
+            logger.info(f"Running {mig.from_rev} -> {mig.to_rev}")
+            await exec.record_migration(
+                from_revision=mig.from_rev,
+                to_revision=mig.to_rev,
+            )
+            await exec.execute_operation(mig.operation)
+            logger.info(f"{mig.from_rev} -> {mig.to_rev} OK")
